45ed90a65b815beddf05eb99a0f42fefb24d4922
BarunMandal016 <barun.mandalbct@gmail.com>
2025-11-13 16:35:58 +0545

    added useid hooks for id generate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
+import { useId } from "react"
+
 export default function Home() {
+  const id = useId()
+  console.log("Unique ID:", id)
   return <div>Learn by creating the branches from this templates</div>
 }
